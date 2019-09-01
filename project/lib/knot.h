04b8f0088e31580cc8f3c603f0ac091f5fdd3df1
Geovanne <geovanneoliveira3@gmail.com>
2019-08-31 21:52:30 -0300

    :rocket: published to npm

---
+++ project/lib/knot.h
@@ -0,0 +1,15 @@
+#ifndef KNOT_H_
+#define KNOT_H_
+
+#include <zephyr.h>
+#include <net/net_core.h>
+#include <logging/log.h>
+
+#include <device.h>
+#include <gpio.h>
+
+#include "knot.h"
+#include <knot/knot_types.h>
+#include <knot/knot_protocol.h>
+
+#endif
